a4f2c824d253b880ca3b50b7a44cfa2e9d72725a
bhetman <brandon@hplappy>
2012-06-19 00:35:16 -0400

    Updates to comments

--- install.sh
+++ install.sh
@@ -3,13 +3,15 @@
 # This script symlinks the dotfiles in this repo into their proper places.
 
 # There are only a few files we need to link, so iterating over this variable
-# will do. If this install procedure gets any more complex a new method should
-# be found.
+# will do. If this install procedure gets more complex a better method should be
+# found.
 FILES=".vimrc
 .zshrc
 .zshenv
 .nethackrc"
 
+# Loop over the files and (after checking for existence) link the file into the
+# home directory.
 for file in $FILES
 do
   if [[ ! -e $file ]]; then
