2ed963536ebf5af417d0c90720f3c8fab14e55de
bkh <bkh@vbox>
2013-09-21 07:14:32 +0000

    Make install.sh more flexible.
    Now the destination directory can be specified for each source file. Now
    that this new functionality is in place, this commit also moves my i3
    config to a more sensible place

--- install.sh
+++ install.sh
@@ -1,31 +1,43 @@
-#!/bin/bash
+#!/bin/zsh
 # A rudimentary install script.
 # This script symlinks the dotfiles in this repo into their proper places.
 
-# There are only a few files we need to link, so iterating over this variable
-# will do. If this install procedure gets more complex a better method should be
-# found.
-FILES=".vimrc
-.zshrc
-.zshenv
-.nethackrc
-.Xresources
-.xinitrc
-.Xmodmap
-.xbindkeysrc"
+FILES=(.vimrc .zshrc .zshenv .nethackrc .Xresources .xinitrc .Xmodmap
+  .xbindkeysrc i3)
+typeset -A destdir
+destdir=(i3 $HOME/.config)
 
-# Loop over the files and (after checking for existence) link the file into the
-# home directory.
+zparseopts -D -A opts -- v -verbose
+
+if (( ${+opts[-v]} || ${+opts[--verbose]} )); then
+  debug=1
+fi
+# Loop over the files and (after ensuring the destination doesn't already exist)
+# link the file into $destdir if specified, falling back to $HOME otherwise.
 for file in $FILES
 do
-  if [[ ! -e "$HOME/$file" ]]; then
-    echo -n "Linking $file..."
-    ln -s "$PWD/$file" "$HOME"
+  dest=$HOME
+  if [[ -n $destdir[$file] ]]; then
+    dest=$destdir[$file]
+    if [[ ! -e $dest ]]; then
+      if (( $debug )); then
+        echo "Creating $dest"
+      fi
+      mkidr -p $dest
+    fi
+  fi
+  if [[ ! -e $dest/$file ]]; then
+    if (( $debug )); then
+      echo -n "Linking $file..."
+    fi
+    ln -s $PWD/$file $dest
     if [[ ret=$? -gt 0 ]]; then
       echo -e "\tCould not create symlink for $file (ln returned $ret)."
       continue
     fi
-    echo "Success!"
+    if (( $debug )); then
+      echo "Success!"
+    fi
   fi
 done
 
